c55177d7bc7eea805c28442f3980b9fd0695dfd6
sufiblues <ali.hamdani.developer@gmail.com>
2021-01-23 18:38:57 -0600

    adding basic gravity physics

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 574;
+const int FRAME_RATE = 60;
+const int FRAME_DELAY = 1000 / FRAME_RATE;
+
 
 SDL_Window* gWindow;
 SDL_Renderer* gRenderer;
@@ -31,6 +34,8 @@ Controller input;
 
 bool init();
 void updateInputState();
+
+
 
 bool init(){
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -65,16 +70,19 @@ void updateInputState(Controller* input) {
         case SDL_KEYDOWN:
             switch (event.key.keysym.sym) {
             case SDLK_UP:
-                input->up += 1;
+                input->up = 1;
                 break;
             case SDLK_DOWN:
-                input->down += 1;
+                input->down = 1;
                 break;
             case SDLK_LEFT:
-                input->left += 1;
+                input->left = 1;
                 break;
             case SDLK_RIGHT:
-                input->right += 1;
+                input->right = 1;
+                break;
+            case SDLK_a:
+                input->jump = 1;
                 break;
             default:
                 break;
@@ -93,6 +101,8 @@ void updateInputState(Controller* input) {
                 break;
             case SDLK_RIGHT:
                 input->right = 0;
+            case SDLK_a:
+                input->jump = 0;
                 break;
             default:
                 break;
@@ -112,17 +122,121 @@ void updateInputState(Controller* input) {
         }
     }
 }
+
+
+SDL_Color red = { 255,0,0,255 };
+
+void movePlayer(Controller* input, SDL_Rect* box) {
+
+    int horizontal_speed = 5;
+    box->x = box->x + input->right*horizontal_speed;
+    box->x = box->x - input->left*horizontal_speed;
+    box->y = box->y + input->down;
+    box->y = box->y - input->up;
+
+    
+
+}
+
+const int GRAVITY = 5;
+const int TERMINAL_SPEED = 100;
+
+void gravity(SDL_Rect* box) {
+
+    int delta = 10; 
+
+    if ((box->y + box->h) <= SCREEN_HEIGHT) {
+        box->y = box->y + delta;
+    }
+
+
+}
+
+struct MovableObject {
+    SDL_Rect box = { 20,20,20,20 };
+    int velocity_x = 0;
+    int velocity_y = 0; 
+    int acceleration_x = 0;
+    int acceleration_y = 0;
+
+};
+
+MovableObject player;
+
+
+void addJump(MovableObject* player) {
+
+    player->velocity_y = -50;
+}
+
+void addGravity(MovableObject* player) {
+    if (abs(player->velocity_y) <= TERMINAL_SPEED) {
+        player->velocity_y += GRAVITY;
+    }
+}
+
+void physics(MovableObject* player) {
+    player->box.y += player->velocity_y;
+}
+
+void stopVelocityY(MovableObject* player) {
+    player->velocity_y = 0;
+}
+
+bool grounded = false; 
+
+
 int main(int argc, char* args[]) {
 
 	std::cout << init();
 
+    int frame_start;
+    int elapsed_ticks;
+
     while (!input.quit) {
+
+        frame_start = SDL_GetTicks();
+
         updateInputState(&input);
+
+
+
+        if (!grounded) {
+            addGravity(&player);
+            if ((player.box.y + player.box.h) >= SCREEN_HEIGHT) {
+                player.box.y = SCREEN_HEIGHT - player.box.h;
+                stopVelocityY(&player);
+                grounded = true;
+            }
+            else {
+                grounded = false;
+            }
+        }
+        if (input.jump && grounded) {
+            addJump(&player);
+            grounded = false;
+        }
+        
+
+        physics(&player);
+
 
         SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
         SDL_RenderClear(gRenderer);
+
+        SDL_SetRenderDrawColor(gRenderer, red.r, red.g, red.b, red.a);
+        SDL_RenderFillRect(gRenderer, &player.box);
+        
         
         SDL_RenderPresent(gRenderer);
+
+        elapsed_ticks = SDL_GetTicks() - frame_start;
+
+        if (elapsed_ticks  < FRAME_DELAY) {
+            SDL_Delay(FRAME_DELAY - elapsed_ticks);
+        }
+        
+        
     }
 	return 0;
 }
